a7b65790419f573bcc0d3eb074216320b12d9b12
Rusiri Jayalath <rusirijayalath92@gmail.com>
2024-02-02 21:54:44 -0500

    final changes

--- src/main/java/com/rusiri/websocket/WebsocketApplication.java
+++ src/main/java/com/rusiri/websocket/WebsocketApplication.java
@@ -2,9 +2,16 @@ package com.rusiri.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebsocketApplication {
+public class WebsocketApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(WebsocketApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebsocketApplication.class, args);
--- src/main/java/com/rusiri/websocket/user/User.java
+++ src/main/java/com/rusiri/websocket/user/User.java
@@ -1,7 +1,6 @@
 package com.rusiri.websocket.user;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Getter;
